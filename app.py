f5ff9a8a4089aca5ca255b8d601ffdb78caeb5eb
Michael Kennedy <mikeckennedy@gmail.com>
2022-04-29 21:38:18 -0700

    Fix spelling error.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def tell_a_joke():
-    print("Begging to tell a joke!")
+    print("Beginning to tell a joke!")
     joke = random.choice(data.jokes)
     print(f"The joke we are telling is {joke}")
     return flask.render_template('joke.html', joke_text=joke)
