393e6284fffea18cb9bb237d832f069012c83561
Michael Kennedy <michael@talkpython.fm>
2022-04-29 21:15:28 -0700

    Finished new data layer refactoring

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
+from data import jokes
 import random
 
 app = Flask(__name__)
 
-jokes = [
-    "Relationship status? I'll leave the relations to the database.",
-    "How do you get the code for the bank vault? You checkout their branch.",
-    "What diet did the ghost developer go on? Boolean",
-]
 
 
 @app.get("/")
